ab0e0be5f3ab5929f28bde2ae926c4b99fa5510b
fathimafaimin <faimirahman@gmail.com>
2022-01-15 00:40:23 +0530

    ch

--- script.js
+++ script.js
@@ -1,66 +1,80 @@
-let email = document.getElementById("email").value;
-// let errorMail = document.getElementById("errorMail");
+let email = document.getElementById("email");
+let errorMail = document.getElementById("errorMail");
 
 let pwd = document.getElementById("pwd").value;
-// let errorPwd = document.getElementById("errorPwd");
-var filter = /^([\w\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+let errorPwd = document.getElementById("errorPwd");
+
 
 function validate()
 {
-    if(email =="")
-		{
-			alert("please enter your email id.");
-		}
-	else if(pwd=="")
-		{
-        	alert("enter the password");
-		}
-	else if(!filter.test(email))
-		{
-			alert("Enter valid email id.");
-		}
-	else if(pwd.length < 6 || pwd.length > 6)
-		{
-			alert("Password min and max length is 6.");
-		}
-	else
+    let regexp = /^([\w\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+    
+    if(!regexp.test(email.value))
+    {
+        errorMail.innerText = 'Invalid';
+        errorMail.style.color = "green";
+        return false;
+    }
+    else if(pwd=="")
+    {
+        errorPwd.innerHTML = "**Please fill the password";
+        errorPwd.style.color = "red";
+        return false;
+
+    }
+    else if(pwd.length<8){
+        errorPwd.innerHTML = "**Password length must be atleast 8";
+        errorPwd.style.color = "red";
+        return false;
+    }
+    
+    else if(regexp.test(email.value)){
+        errorMail.innerText = "Invalid";
+        errorMail.style.color = "red";
+        return true;
+    }
+    else
 		{
 	    alert('Thank You for Login & You are Redirecting to Kerala Tourism Website');
   
         window.location = "./index.html";
 		}
+
 }
-	//Reset Inputfield code.
 function clearFunc()
 {
 		document.getElementById("email").value="";
 		document.getElementById("pwd").value="";
-}	
+}
 
 
-    // let regexp = /^([\w\.-]+)@([\w\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
-    // if(pwd=="")
-    // {
-    //     errorPwd.innerHTML = "**Please fill the password";
-    //     errorPwd.style.color = "red";
-    //     return false;
 
-    // }
-    // // if(pwd.length<8){
-    // //     errorPwd.innerHTML = "**Password length must be atleast 8";
-    // //     errorPwd.style.color = "red";
-    // //     return false;
-    // //}
-    // if(regexp.test(email.value))
-    // {
-    //     errorMail.innerText = 'valid';
-    //     errorMail.style.color = "green";
-    //     return true;
-    // }
-    // else{
-    //     errorMail.innerText = "Invalid";
-    //     errorMail.style.color = "red";
-    //     return false;
-    // }
+// var filter = /^([\w\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
 
-
+// function validate()
+// {
+//     if(email =="")
+// 		{
+// 			alert("please enter your email id.");
+// 		}
+// 	else if(pwd=="")
+// 		{
+//         	alert("enter the password");
+// 		}
+// 	else if(!filter.test(email))
+// 		{
+// 			alert("Enter valid email id.");
+// 		}
+// 	else if(pwd.length < 6 || pwd.length > 6)
+// 		{
+// 			alert("Password min and max length is 6.");
+// 		}
+// 	else
+// 		{
+// 	    alert('Thank You for Login & You are Redirecting to Kerala Tourism Website');
+  
+//         window.location = "./index.html";
+// 		}
+// }
+	//Reset Inputfield code.
+	
