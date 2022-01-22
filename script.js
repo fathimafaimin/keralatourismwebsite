8e58cd96336ced1c9e2c677e31ada6d0afe2ece8
fathimafaimin <faimirahman@gmail.com>
2022-01-22 17:20:54 +0530

    changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let email = document.getElementById("email").value;
+let email = document.getElementById("email");
 let errorMail = document.getElementById("errorMail");
 
 let pwd = document.getElementById("pwd").value;
@@ -7,58 +7,73 @@ let errorPwd = document.getElementById("errorPwd");
 
 function validate()
 {
-    let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    let regexp = /^([\w\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
     
-    if(regexp.test(email))
+    if((regexp.test(email.value))||(pwd.length>6))
     {
         errorMail.innerText = 'Valid';
-
         errorMail.style.color = "green";
-        // errorPwd.innerHTML = "valid";
-        // errorPwd.style.color = "green";
-        return true;
-    }
-    else if(pwd=="")
-    {
-        // if(pwd.length<5||pwd.length>16){
-        errorPwd.innerHTML = "**Please fill the password";
-        errorPwd.style.color = "red";
-        // alert("**Please fill the password");
-        return false;
-        // }
-    }
-    
-    else
-    {
-        errorMail.innerHTML = "Invalid";
-        errorMail.style.color = "red";
-        return false;
-
-
-    }
-    
-}
- function login(){
-
-    if(pwd==""){
-        errorPwd.innerHTML = "**Please fill the password";
-        errorPwd.style.color = "red";
-        return false;
-
-    }
-    else if(pwd.length<8){
-        errorPwd.innerHTML = "**Password length must be atleast 8";
-        errorPwd.style.color = "red";
-        return false;
-    }
-    else{
         errorPwd.innerHTML = "valid";
         errorPwd.style.color = "green";
         return true;
-
+        
+        // if(pwd.trim()=="")
+            // {
+                // if(pwd.length<5||pwd.length>16){
+                // errorPwd.innerHTML = "**Please enter password";
+                // errorPwd.style.color = "red";
+                // alert("**Please fill the password");
+                // }
     }
+    else
+    {
+        errorMail.innerText = 'Inalid';
+        errorMail.style.color = "red";
+        errorPwd.innerHTML = "**Password length must be atleast 6";
+        errorPwd.style.color = "red";
+        return false;
+    }
+        // errorPwd.innerHTML = "valid";
+        // errorPwd.style.color = "green";
+        
+        // return false;
+    
+    
+    
+    // else
+    // {
+    //     errorMail.innerHTML = "Valid";
+    //     errorMail.style.color = "green";
 
- }   
+    //     errorPwd.innerHTML = "valid";
+    //     errorPwd.style.color = "green";
+    //     return true;
+
+
+    // }
+    
+}
+//  function login(){
+
+//     if(pwd==""){
+//         errorPwd.innerHTML = "**Please fill the password";
+//         errorPwd.style.color = "red";
+//         return false;
+
+//     }
+//     else if(pwd.length<8){
+//         errorPwd.innerHTML = "**Password length must be atleast 8";
+//         errorPwd.style.color = "red";
+//         return false;
+//     }
+//     else{
+//         errorPwd.innerHTML = "valid";
+//         errorPwd.style.color = "green";
+//         return true;
+
+//     }
+
+//  }   
 
     
     
